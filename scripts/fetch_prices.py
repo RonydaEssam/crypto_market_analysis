eae47991f71b775f341df886b947dded619ebd61
RonydaEssam <ronyda988@gmail.com>
2025-05-23 14:48:11 +0300

    Update script file

--- scripts/fetch_prices.py
+++ scripts/fetch_prices.py
@@ -1,6 +1,8 @@
 import requests
 import os
 from dotenv import load_dotenv
+import pandas as pd
+from datetime import datetime
 
 # load API_KEY from .env file
 load_dotenv()
@@ -22,8 +24,23 @@ params = {
 response = requests.get(url, headers=headers, params=params)
 data = response.json()
 
-for coin in data["data"]:
-    name = coin["name"]
-    price = coin["quote"]["USD"]["price"]
-    print(f"{name}: ${price:,.3f}")
 
+
+coins = []
+
+for coin in data['data']:
+    coin_info = {
+        "name": coin["name"],
+        "symbol": coin["symbol"],
+        "price_usd": coin["quote"]["USD"]["price"],
+        "percent_change_24h": coin["quote"]["USD"]["percent_change_24h"],
+        "market_cap_usd": coin["quote"]["USD"]["market_cap"]
+    }
+    coins.append(coin_info)
+
+df = pd.DataFrame(coins)
+print(df.head())
+
+today = datetime.today().strftime('%Y-%m-%d')
+filename = f"D:/0.0 Data Engineering/Projects/Crypto_market_analysis/crypto_market_analysis/data/prices_{today}.csv"
+df.to_csv(filename, index=False)
