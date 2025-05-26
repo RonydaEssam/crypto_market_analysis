b694b80cde4b8c40b65d48ac5edfc879b3e124a6
RonydaEssam <ronyda988@gmail.com>
2025-05-26 20:54:46 +0300

    fixing file path in script

--- scripts/dashboard.py
+++ scripts/dashboard.py
@@ -3,7 +3,7 @@ import streamlit as st
 from matplotlib import pyplot as plt
 
 #loading the data to pandas df
-df = pd.read_csv("D:/0.0 Data Engineering/Projects/Crypto_market_analysis/crypto_market_analysis/data/combined_data.csv")
+df = pd.read_csv("../data/combined_data.csv")
 
 #creating streamlit sidebar
 st.sidebar.title("Crypto Dashboard")
